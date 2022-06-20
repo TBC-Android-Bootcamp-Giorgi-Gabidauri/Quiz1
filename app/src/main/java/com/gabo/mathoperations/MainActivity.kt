477ec7695e9b0ab051a7faf88f9514a988bb8a11
Giorgi Gabidauri <giorgi.gabidauri16@gmail.com>
2022-06-20 12:33:52 +0400

    sorry for my mistake that functions were private and out of math class.... i will never do that again promise.... <3
    ui implemented

--- app/src/main/java/com/gabo/mathoperations/MainActivity.kt
+++ app/src/main/java/com/gabo/mathoperations/MainActivity.kt
@@ -2,10 +2,58 @@ package com.gabo.mathoperations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.gabo.mathoperations.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val math = MathOperations()
+        with(binding) {
+            btnLCD.setOnClickListener {
+                if (etFirstNum.text != null && etSecondNum.text != null &&
+                    etFirstNum.text.toString() != "" && etSecondNum.text.toString() != "" ) {
+                    tvAnswerTwoNumber.text = math.largestCommonDivider(
+                        etFirstNum.text.toString().toInt(),
+                        etSecondNum.text.toString().toInt()
+                    ).toString()
+                } else {
+                    Toast.makeText(this@MainActivity, "something went wrong", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+            }
+            btnLCM.setOnClickListener {
+                if (etFirstNum.text != null && etSecondNum.text != null) {
+                    tvAnswerTwoNumber.text = math.leastCommonMultiple(
+                        etFirstNum.text.toString().toInt(),
+                        etSecondNum.text.toString().toInt()
+                    ).toString()
+                } else {
+                    Toast.makeText(this@MainActivity, "something went wrong", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+            btnIsPalindrome.setOnClickListener {
+                tvAnswer.text = math.isPalindrome(etInput.text.toString()).toString()
+            }
+            btnReverce.setOnClickListener {
+                if (etInput.text != null && etInput.text.toString().toIntOrNull() is Int) {
+                    tvAnswer.text = math.reverseNumber(etInput.text.toString().toInt())
+                } else {
+                    Toast.makeText(this@MainActivity, "something went wrong", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+            btnSumEvenNumbers.setOnClickListener {
+                tvAnswer.text = math.sumEvenNumbers(100).toString()
+            }
+            btnContains.setOnClickListener {
+                tvAnswer.text = math.containsOrNot(etInput.text.toString()).toString()
+            }
+        }
     }
 }
--- app/src/main/java/com/gabo/mathoperations/MathOperations.kt
+++ app/src/main/java/com/gabo/mathoperations/MathOperations.kt
@@ -1,82 +1,80 @@
 package com.gabo.mathoperations
 
-class MathOperations {
-}
-
-fun main() {
-    println(largestCommonDivider(24, 36))
-    println(leastCommonMultiple(24,36))
-    println(containsOrNot("1234"))
-    println(containsOrNot("1234$"))
-    println(addEvenNumbers(100))
-    println(reverseNumber(1234567800))
-    println(isPalindrome("12343"))
-    print(isPalindrome("123454321"))
-}
-
-private fun largestCommonDivider(n1: Int, n2: Int): Int {
-    var i = if (n1 > n2) {
-        n1
-    } else {
-        n2
-    }
-    var answer = 1
-    while (i > 0) {
-        if (n1 % i == 0 && n2 % i == 0) {
-            answer = i
-            break
+class MathOperations() {
+     fun largestCommonDivider(n1: Int, n2: Int): Int {
+        var i = if (n1 > n2) {
+            n1
         } else {
-            i--
+            n2
         }
+        var answer = 1
+        while (i > 0) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                answer = i
+                break
+            } else {
+                i--
+            }
+        }
+        return answer
     }
-    return answer
-}
 
-private fun leastCommonMultiple(n1: Int, n2: Int): Int {
-    var i = if (n1 > n2) {
-        n1
-    } else {
-        n2
-    }
-    var answer = 1
-    while (i > 1) {
-        if (i % n1 == 0 && i % n2 == 0) {
-            answer = i
-            break
+     fun leastCommonMultiple(n1: Int, n2: Int): Int {
+        var i = if (n1 > n2) {
+            n1
         } else {
-            i++
+            n2
         }
+        var answer = 1
+        while (i > 1) {
+            if (i % n1 == 0 && i % n2 == 0) {
+                answer = i
+                break
+            } else {
+                i++
+            }
+        }
+        return answer
     }
-    return answer
+
+     fun containsOrNot(input: String): Boolean {
+        var contains = false
+        if(input.contains('$')){
+            contains = true
+        }
+        return contains
+    }
+     fun sumEvenNumbers(num: Int): Int {
+        if (num != 0 && num % 2 == 0)
+            return num + sumEvenNumbers(num - 2)
+        else
+            return num
+    }
+     fun reverseNumber(input: Int) : String{
+        var answer = input.toString()
+        while(answer[answer.lastIndex] == '0'){
+            answer = answer.dropLast(1)
+        }
+        return answer.reversed()
+    }
+     fun isPalindrome(input: String): Boolean{
+        var answer = false
+        if (input == input.reversed()){
+            answer = true
+        }
+        return answer
+    }
 }
 
-private fun containsOrNot(input: String): Boolean {
-    var contains = false
-    if(input.contains('$')){
-        contains = true
-    }
-    return contains
-}
-private fun addEvenNumbers(num: Int): Int {
-    if (num != 0 && num % 2 == 0)
-        return num + addEvenNumbers(num - 2)
-    else
-        return num
-}
-private fun reverseNumber(input: Int) : String{
-    var i = 0
-    var answer = input.toString()
-   while(answer[answer.lastIndex] == '0'){
-       answer = answer.dropLast(1)
-   }
-       return answer.reversed()
-}
-private fun isPalindrome(input: String): Boolean{
-    var answer = false
-    if (input == input.reversed()){
-        answer = true
-    }
-    return answer
-}
+//fun main() {
+//    println(largestCommonDivider(24, 36))
+//    println(leastCommonMultiple(24,36))
+//    println(containsOrNot("1234"))
+//    println(containsOrNot("1234$"))
+//    println(sumEvenNumbers(100))
+//    println(reverseNumber(1234567800))
+//    println(isPalindrome("12343"))
+//    print(isPalindrome("123454321"))
+//}
 
 
